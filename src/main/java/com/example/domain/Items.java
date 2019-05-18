b9d24fa58521d5e3fe555ee84df0bd8f1dc2dc52
NitishKumarNirala <nitishkumarnirala1992@gmail.com>
2019-05-19 04:18:38 +0530

    services

---
+++ src/main/java/com/example/domain/Items.java
@@ -0,0 +1,27 @@
+package com.example.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Items {
+
+	 public Items(String name) {
+	        this.name = name;
+	    }
+
+	    @Id
+	    @GeneratedValue
+	    private Long id;
+
+	    private String name;
+}
---
+++ src/main/java/com/example/repo/ItemInitializer.java
@@ -0,0 +1,25 @@
+package com.example.repo;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.domain.Items;
+
+@Component
+public class ItemInitializer implements CommandLineRunner {
+
+	private final ItemRepository itemRepository;
+
+	ItemInitializer(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	@Override
+	    public void run(String... args) throws Exception {
+	        Stream.of("Lining", "PUMA", "Bad Boy", "Air Jordan", "Nike", "Adidas", "Reebok")
+	                .forEach(item -> itemRepository.save(new Items(item)));
+	        itemRepository.findAll().forEach(System.out::println);
+	    }
+}
